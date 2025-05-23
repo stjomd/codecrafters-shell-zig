81083328ceab4a609b864a43547d1416f78ea371
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-05-23 20:55:06 +0200

    add exit command

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,26 @@
 const std = @import("std");
 
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
+pub fn main() !void {
+    var buffer: [1024]u8 = undefined;
     while (true) {
         try stdout.print("$ ", .{});
-        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-        try stdout.print("{s}: command not found\n", .{user_input});
+        const input = try stdin.readUntilDelimiter(&buffer, '\n');
+        try command(input);
     }
+}
+
+fn command(input: []u8) !void {
+    if (std.mem.startsWith(u8, input, "exit")) {
+        try exit(input[4..]);
+    } else {
+        try stdout.print("{s}: command not found\n", .{input});
+    }
+}
+
+fn exit(args: []u8) !void {
+    const code = try std.fmt.parseInt(u8, args[1..], 10);
+    std.process.exit(code);
 }
